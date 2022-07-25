cd8f3de1d229f75e33aee5d5e14e1512ff83416f
Tl26 <100012608+Tl26@users.noreply.github.com>
2022-07-25 18:51:05 +0530

    initial commit

--- Main.py
+++ Main.py
@@ -1,15 +1,15 @@
 class Evaluate:
  #This class validates and evaluate postfix expression.
-  #Attributes:
-   #   top: An integer which denotes the index of the element at the top of the stack currently.
-    #  size_of_stack: An integer which represents the size of stack.
-     # stack: A List which acts as a Stack.
+ #Attributes:
+ #   top: An integer which denotes the index of the element at the top of the stack currently.
+ #   size_of_stack: An integer which represents the size of stack.
+ #   stack: A List which acts as a Stack.
 
 
   def __init__(self, size):
   #Inits Evaluate with top, size_of_stack and stack.
- #   Arguments:
-#      size_of_stack: An integer to set the size of stack.
+  #  Arguments:
+  #    size_of_stack: An integer to set the size of stack.
     self.top = -1
     self.size_of_stack = size
     self.stack = []
@@ -18,15 +18,15 @@ class Evaluate:
   def isEmpty(self):
     #Check whether the stack is empty.
     #Returns:
-      #True if it is empty, else returns False
+    # True if it is empty, else returns False
       return len(self.stack) == 0
 
 
   def pop(self):
     #Do pop operation if the stack is not empty.
     #Returns:
-      #The data which is popped out if the stack is not empty.
-    if not self.is_empty():
+    # The data which is popped out if the stack is not empty.
+    if not self.isEmpty():
       self.top -= 1
       return self.stack.pop(-1)
     
@@ -34,8 +34,8 @@ class Evaluate:
 
   def push(self, operand):
    #Push the operand to stack if the stack is not full.
-    #Arguments:
-      #operand: The operand to be pushed.
+   #Arguments:
+   # operand: The operand to be pushed.
     if len(self.stack) != self.size_of_stack:
       self.top += 1
       self.stack.append(data)
@@ -45,22 +45,21 @@ class Evaluate:
   def validate_postfix_expression(self, expression):
     #Check whether the expression is a valid postfix expression.
     #Arguments:
-     # expression: A String which represents the expression to be validated.
+    #  expression: A String which represents the expression to be validated.
     #Returns:
-      #True if the expression is valid, else returns False
+    #  True if the expression is valid, else returns False
     elements = expression.split()
     operands = [element if element.isdigit() for element in elements]
     operators = [element if element in ["+", "-", "*", "/", "^"] for element in elements]
-    equal_operators_and_operands = (len(operands) + len(operators)) == len(elements)
-    return equal_operators_and_operands
+    return (len(operands) + len(operators)) == len(elements)
 
 
   def evaluate_postfix_expression(self, expression):
     #Evaluate the postfix expression
     #Arguments:
-#      expression: A String which represents the the expression to be evaluated
-#    Returns:
- #     The result of evaluated postfix expression.
+    #    expression: A String which represents the the expression to be evaluated
+    #Returns:
+    #    The result of evaluated postfix expression.
     self.stack = []
     elements = expression.split()
     final_result = 0
